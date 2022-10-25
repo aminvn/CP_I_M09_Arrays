41d0e4fc478bc56066c91e47f44bfbef806b5412
Vedi Amin <vediamin@vedis-mbp.wifi.uc.edu>
2022-10-25 14:04:55 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        String[] names = {"Bob", "Carol", "Ted", "Alice"};
+        System.out.println(names.length);
+    }
+}
